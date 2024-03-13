0f7b58b224b573143d3d079f4511fe27b643dd24
harshil Sharma <harshilsharma63@gmail.com>
2024-03-13 18:24:35 +0530

    Added logic to save custom setting, added CSS changes

--- webapp/src/components/surveyTimeSelector/surveyTimeSelector.tsx
+++ webapp/src/components/surveyTimeSelector/surveyTimeSelector.tsx
@@ -1,21 +1,30 @@
-import React, {useMemo} from 'react';
-import Dropdown from 'components/common/dropdown/dropdown';
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React, {useCallback, useMemo, useState} from 'react';
+
+import Dropdown, {DropdownOption} from 'components/common/dropdown/dropdown';
 
 import './style.scss';
 
 export type Props = {
     value?: string
+    onChange?: (value: string) => void
 };
 
-const SurveyTimeSelector = ({}: Props) => {
+const SurveyTimeSelector = ({value, onChange}: Props) => {
     const options = useMemo(() => {
-        const timeStrings = [];
+        // we need to generate 24 hour format time from 00:00 (12 am night)
+        // to 23:30 (11:30 PM) in 30 minute increment
+
+        const timeStrings: DropdownOption[] = [];
 
         for (let hours = 0; hours < 24; hours++) {
             for (let minutes = 0; minutes <= 30; minutes += 30) {
                 const hourString = String(hours).padStart(2, '0');
                 const minuteString = String(minutes).padStart(2, '0');
                 const timeString = `${hourString}:${minuteString}`;
+
                 timeStrings.push({value: timeString, label: timeString});
             }
         }
@@ -23,10 +32,22 @@ const SurveyTimeSelector = ({}: Props) => {
         return timeStrings;
     }, []);
 
+    const [selectedValue, setSelectedValue] = useState<DropdownOption>(value ? {value, label: value} : options[18]);
+
+    const onChangeHandler = useCallback((newValue: DropdownOption) => {
+        setSelectedValue(newValue);
+
+        if (onChange) {
+            onChange(newValue.value);
+        }
+    }, [onChange]);
+
     return (
         <div className='SurveyTimeSelector'>
             <Dropdown
+                value={selectedValue}
                 options={options}
+                onChange={onChangeHandler}
             />
         </div>
     );
