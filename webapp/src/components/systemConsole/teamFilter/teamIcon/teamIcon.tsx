140e16f9972a33e105269f33bb92a38cfa32969a
harshil Sharma <harshilsharma63@gmail.com>
2024-03-28 09:51:06 +0530

    Minor touchups

--- webapp/src/components/systemConsole/teamFilter/teamIcon/teamIcon.tsx
+++ webapp/src/components/systemConsole/teamFilter/teamIcon/teamIcon.tsx
@@ -19,6 +19,10 @@ function TeamIcon({data}: Props) {
     // field will be included in latest types release,
     // but its there in the data already
     if (data.raw.last_team_icon_update) {
+        // last_team_icon_update is set whenever a team icon is set or updated.
+        // So, a truthy value of `last_team_icon_update` field indicates precense of
+        // team icons, so here we use it to show the team icon image.
+
         // @ts-expect-error sane reason as above about the last_team_icon_update field
         const teamURL = Client4.getTeamIconUrl(data.value, (data.raw as Team)?.last_team_icon_update);
         return (
@@ -32,10 +36,13 @@ function TeamIcon({data}: Props) {
             </div>
         );
     }
+
+    // if a team icon isn't available, we use the first
+    // two letters of team's display name as a placeholder icon.
     return (
         <div className='customOption_imageContainer'>
             <div className='teamImage'>
-                <span className='teamImagePlaceholder'>{data.label[0]}</span>
+                <span className='teamImagePlaceholder'>{data.label.substring(0, 2)}</span>
             </div>
         </div>
     );
