7d6388ba0e35bf50abf5487815bd78a2c13bd171
Harshil Sharma <harshilsharma63@gmail.com>
2024-05-16 10:49:56 +0530

    Updated end survey text

--- webapp/src/components/systemConsole/surveyResults/surveyRowActionButton.tsx
+++ webapp/src/components/systemConsole/surveyResults/surveyRowActionButton.tsx
@@ -76,9 +76,9 @@ export default function SurveyRowActionButton({surveyResult, onStopSurvey}: Prop
                 showConfirmationModal &&
                 <ConfirmationModal
                     id='endSurveyConfirmationModal'
-                    title='Are you sure you want to end this survey?'
-                    bodyMessage='Reprehenderit aute laborum anim dolore magna mollit incididunt officia qui labore. Esse culpa ipsum ut esse fugiat do tempor duis mollit. Labore nisi nisi ut aliquip enim aute ex. Esse veniam proident id dolor laborum occaecat voluptate esse laborum cupidatat aliquip ut laborum id. Laborum nostrud officia excepteur. Consectetur ad nisi ullamco est cillum proident Lorem. Aliqua eu est magna deserunt est elit elit do.'
-                    confirmButtonText='End active survey'
+                    title='Confirm survey end'
+                    bodyMessage="Are you sure you want to end this active survey? Once the survey ends, it can't be re-started, and no new responses can be collected"
+                    confirmButtonText='End survey'
                     handleConfirm={handleStopSurvey}
                     handleCancel={handleHideConfirmationDialog}
                 />
