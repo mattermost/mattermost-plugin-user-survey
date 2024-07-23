8bdee5896749529a94de7ec7daac85dd8d45ceb4
Harshil Sharma <harshilsharma63@gmail.com>
2024-07-23 08:00:14 +0530

    Used tradition SQL comments

--- server/store/migrations/000014_survey_table_add_team_filter_type_column.up.sql
+++ server/store/migrations/000014_survey_table_add_team_filter_type_column.up.sql
@@ -1,6 +1,6 @@
-{{- /* you annot add a new colum with not null constraint without a default value */ -}}
-{{- /* so, we're adding the column and setting the default value we need to set in existing rows */ -}}
-{{- /* then removing the default value from column as its no longer needed */ -}}
+-- you annot add a new colum with not null constraint without a default value
+-- so, we're adding the column and setting the default value we need to set in existing rows
+-- then removing the default value from column as its no longer needed
 
 {{ addColumnIfNeeded "survey" "team_filter_type" "varchar(128)" "NOT NULL DEFAULT 'exclude_selected'"}}
 
