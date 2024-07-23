2a7922e15808a646fdd1341ba7e33ed9ac7eb309
Harshil Sharma <harshilsharma63@gmail.com>
2024-07-23 08:01:00 +0530

    Fixed typo

--- server/store/migrations/000014_survey_table_add_team_filter_type_column.up.sql
+++ server/store/migrations/000014_survey_table_add_team_filter_type_column.up.sql
@@ -1,4 +1,4 @@
--- you annot add a new colum with not null constraint without a default value
+-- you cannot add a new column with not null constraint without a default value
 -- so, we're adding the column and setting the default value we need to set in existing rows
 -- then removing the default value from column as its no longer needed
 
